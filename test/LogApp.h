7e58559fcc74d34bc9a2a4f037d3bdef1052113b
Balazs Bamer <balazs@nowtech.hu>
2019-05-15 09:26:20 +0200

    Added single-thread std::ostream interface. Added missing example LogApp.h

---
+++ test/LogApp.h
@@ -0,0 +1,15 @@
+#ifndef NOWTECH_LOGAPP_H_INCLUDED
+#define NOWTECH_LOGAPP_H_INCLUDED
+
+#include <cstdint>
+
+namespace nowtech {
+
+  enum class LogApp : uint8_t {
+    cInvalid,
+    cSystem
+  };
+
+}
+
+#endif /* NOWTECH_LOGAPP_H_INCLUDED */
